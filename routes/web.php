c2a5d895d4e0ad958ff73a2cf51bb65b7eaaf4d0
Ohtaki_endless <orora_togiants@yahoo.co.jp>
2021-09-25 13:49:10 +0900

    投稿、編集、削除機能追加

--- routes/web.php
+++ routes/web.php
@@ -11,4 +11,10 @@
 |
 */
 
-Route::get('/posts', 'PostController@index');
+Route::get('/', 'PostController@index');
+Route::get('/posts/create', 'PostController@create');
+Route::get('/posts/{post}/edit', 'PostController@edit');
+Route::get('/posts/{post}', 'PostController@show');
+Route::put('/posts/{post}', 'PostController@update');
+Route::post('/posts', 'PostController@store');
+Route::delete('/posts/{post}', 'PostController@delete');
