7223493af3c65642b2ab462f43b851eaf1469988
Ohtaki_endless <orora_togiants@yahoo.co.jp>
2021-11-21 20:15:41 +0900

    update google login

--- config/services.php
+++ config/services.php
@@ -33,6 +33,6 @@ return [
     'google' => [
         'client_id' => env('GOOGLE_CLIENT_ID'),
         'client_secret' => env('GOOGLE_CLIENT_SECRET'),
-	    'redirect' => env('GOOGLE_CALLBACK_URL'),
+	    'redirect' => env('APP_URL') . '/login/google/callback'
     ],
 ];
